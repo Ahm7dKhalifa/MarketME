7cd5f601d2a1f990d862ab31d451adfce9cada9f
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-05 15:07:14 +0200

    add dreamOne and dreamTwo Scenes

---
+++ 360VR_Style/Assets/_APP/Scripts/GoHerePoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoHerePoint : MonoBehaviour {
+
+    public Animator scaleAnimation;
+    public string NextSceneName;
+	// Use this for initialization
+	void Start () {
+        scaleAnimation.GetComponents<Animator>();
+    }	
+    public void StartScaleGoHere()
+    {
+        scaleAnimation.Play("GoHereAnimation");
+    }
+    public void StopScaleGoHere()
+    {
+        scaleAnimation.Play("none");
+        
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }
+
+}
---
+++ 360VR_Style/Assets/_APP/Scripts/ToggleChild.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleChild : MonoBehaviour {
+
+    public GameObject parent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //DisableAllChild
+    public void DisableAllChild()
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            var child = parent.transform.GetChild(i).gameObject;
+            if (child != null)
+                child.SetActive(false);
+        }
+    }
+    //EnableAllChild
+    public void EnableAllChild()
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            var child = parent.transform.GetChild(i).gameObject;
+            if (child != null)
+                child.SetActive(true);
+        }
+    }
+}
