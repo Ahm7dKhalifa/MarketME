e7bb7904990990d5a90ab79ec12788baf2c031bc
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-04-05 20:14:05 +0200

    add DepartmentsOfPlaces Scene

---
+++ 360VR_Style/Assets/_APP/Scripts/DepartmentPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DepartmentPoint : MonoBehaviour {
+
+    public Animator scaleAnimation;
+    public string animationName;
+    public string NextSceneName;
+    // Use this for initialization
+    void Start()
+    {
+        scaleAnimation.GetComponents<Animator>();
+    }
+    public void StartScaleGoHere()
+    {
+        scaleAnimation.Play(animationName);
+       
+    }
+    public void StopScaleGoHere()
+    {
+        scaleAnimation.Play("none");
+
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }
+}
